844ccdc8649f50eb97253c4e3a09b122c8c2126f
Gurpreet Singh <gdhuper@gmail.com>
2016-11-06 08:58:47 -0800

    Adding Comments

--- FIFO.java
+++ FIFO.java
@@ -5,7 +5,7 @@ import java.util.List;
  */
 public class FIFO implements ReplacementAlgorithm {
 	
-	public  int idxToRemove =0;
+	public  int idxToRemove =0; //variable to keep track of next  idx to remove
     @Override
 	public Page findPageToReplace(List<Page> occupiedPages) {
     	
@@ -20,7 +20,7 @@ public class FIFO implements ReplacementAlgorithm {
         	{	
 		page =  occupiedPages.remove(idxToRemove++);
         	}
-         if(idxToRemove == 100) {
+         if(idxToRemove == 100) { //if variable reaches 100 start from 0
         	page =  occupiedPages.remove(0);
          	}
         }
