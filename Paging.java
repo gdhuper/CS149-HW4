2d2a025c57cd540df7150ef380a696e5a1dcb63c
JaylanT <Jaylan.Tse@gmail.com>
2016-11-07 17:43:43 -0800

    Some changes to stop execution of child timers after main timer stops.

--- Paging.java
+++ Paging.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Paging {
 
@@ -11,11 +12,9 @@ public class Paging {
     private final List<Process> runningProcesses;
     private final List<Page> occupiedPagesList;
     private final Random random = new Random();
-    private boolean childThreadFinished = false;
-
-   
 
 	private int finishedProcessCount;
+    private AtomicBoolean continueExecuting;
 
     public Paging(int memorySize, int pageSize, int minPagesRequired, ReplacementAlgorithm alg) {
         this.minPagesRequired = minPagesRequired;
@@ -28,7 +27,7 @@ public class Paging {
         occupiedPagesList = Collections.synchronizedList(new LinkedList<Page>()); // List of all pages that reference some processes' page
         
         finishedProcessCount = 0; //keep track of finished processes
-        childThreadFinished = false;
+        continueExecuting = new AtomicBoolean(true);
         // Initialize free pages
         for (int i = 0; i < pagesCount; i++)
             freePagesList.add(new Page(i));
@@ -44,18 +43,12 @@ public class Paging {
         return minPagesRequired * runningProcesses.size() >= pagesCount;
     }
     
-    
     public int getFinishedProcessCount() {
     	return this.finishedProcessCount;
     }
-    
-    
-    public boolean isChildThreadFinished() {
-		return childThreadFinished;
-	}
 
-	public void setChildThreadFinished(boolean childThreadFinished) {
-		this.childThreadFinished = childThreadFinished;
+	public void stopExecution() {
+        continueExecuting.set(false);
 	}
 
     /**
@@ -73,27 +66,24 @@ public class Paging {
 
             @Override
             public void run() {
+                if (!continueExecuting.get())
+                    timer.cancel();
+
                 final long elapsedTime = System.currentTimeMillis() - t0;
 
                 // Run each process for its service duration
-                if (elapsedTime >= process.getServiceDuration() * 1000 ) {
+                if (elapsedTime >= process.getServiceDuration() * 1000) {
                     // Process is finished
                     System.err.printf("%s (SIZE: %d, DURATION: %.0f) EXIT: %.2fsec\n%s\n",
                             process.getName(), process.getPageCount(), process.getServiceDuration(),
                             (startTime + elapsedTime) / 1000.0, Paging.this.toString());
                     runningProcesses.remove(process);
                     finishedProcessCount++;
+
                     timer.cancel();
-                    timer.purge();
                 } else {
                     // Every 100 msec make a memory reference to another page in that process
-                	if(isChildThreadFinished() == false){
                     referencePage(process);
-                	}
-                	else
-                	{
-                		 timer.cancel(); 
-                	}
                 }
             }
         }, 0, 100);
@@ -108,12 +98,12 @@ public class Paging {
         // Get page from freePagesList or find a page to swap out
         Page page = freePagesList.isEmpty() ? findPageToSwap() : freePagesList.remove();
 
-        if (page != null) {
+        if (page != null && continueExecuting.get()) {
             System.out.println("Referencing page for " + process.getName() + ": 0");
             process.setPageReferenced(0, page);
             page.setReferencedProcess(process);
             // Add process to page map
-            updatePageMap(page.getNumber(), process);
+            updatePageMap(page, process);
             System.out.println(this.toString());
 
             occupiedPagesList.add(page);
@@ -135,12 +125,12 @@ public class Paging {
         // Get page from freePagesList or find a page to swap out
         Page page = freePagesList.isEmpty() ? findPageToSwap() : freePagesList.remove();
 
-        if (page != null) {
+        if (page != null && continueExecuting.get()) {
             System.out.println("Referencing page for " + process.getName() + ": " + pageToRefer);
             process.setPageReferenced(pageToRefer, page);
             page.setReferencedProcess(process);
             // Add process to page map
-            updatePageMap(page.getNumber(), process);
+            updatePageMap(page, process);
             System.out.println(this.toString());
             
             if(page.getIdxLRU() != -1) //if LRU is used as swapping algorithm
@@ -165,8 +155,7 @@ public class Paging {
             final Process referencedProcess = pageToSwap.getReferencedProcess();
             referencedProcess.dereferencePage(pageToSwap.getReferencedPage());
             // Remove process from page map
-           
-            	 updatePageMap(pageToSwap.getNumber(), null);
+            updatePageMap(pageToSwap, null);
             
 //            System.out.println(this.toString());
 
@@ -194,7 +183,7 @@ public class Paging {
                 nextPage += pagesCount;
         } else {
             // Else change in i is 2 to process page count (exclusive)
-            int[] ex = { lastReferencedPage - 1, lastReferencedPage, lastReferencedPage + 1 };
+            final int[] ex = { lastReferencedPage - 1, lastReferencedPage, lastReferencedPage + 1 };
             nextPage = getRandomWithExclusion(0, pagesCount, ex);
         }
 
@@ -203,12 +192,12 @@ public class Paging {
 
     /**
      * Updates the page map with a process or null.
-     * @param pageNumber the page number that has been updated
+     * @param page the page that has been updated
      * @param process the process or null that the now page refers to
      */
-    private void updatePageMap(int pageNumber, Process process) {
+    private void updatePageMap(Page page, Process process) {
         synchronized (pageMap) {
-            pageMap[pageNumber] = process;
+            pageMap[page.getNumber()] = process;
         }
     }
 
@@ -240,7 +229,6 @@ public class Paging {
                 } else {
                     sb.append(".");
                 }
-                sb.append(" ");
             }
         }
 
